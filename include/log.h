0d1f4e24944c89cb176e4ae8f348bb42aeada828
zerons <sironhide0null@gmail.com>
2016-06-21 11:13:02 +0800

    modify log.c log.h

--- include/log.h
+++ include/log.h
@@ -5,16 +5,15 @@
 #include <fcntl.h>
 #include <sys/types.h>
 #include "./error.h"
-#include "string.h"
-#include "list.h"
+#include "./string.h"
+#include "./list.h"
+#include "./file.h"
 
-typedef struct _log_file {
-	int fd;
-	const char *path;
-} log_file;
+typedef text_file log_file;
+typedef enum _log_level {debug, info, warn, error, fatal} log_level;
 
 extern log_file *log_open(char *path);
-extern int log_write(log_file *file, char *fmt, ...);
+extern int log_write(log_file *file, log_level level, char *fmt, ...);
 extern int log_close(log_file *file);
 
 #endif
--- src/log.c
+++ src/log.c
@@ -2,33 +2,10 @@
 
 log_file *log_open(char *path)
 {
-	if (!path) {
-		err_dbg(0, err_fmt("arg check err"));
-		errno = EINVAL;
-		return NULL;
-	}
-
-	int fd;
-	fd = open(path, O_WRONLY | O_CREAT | O_APPEND, S_IRUSR | S_IWUSR);
-	if (fd == -1) {
-		err_dbg(1, err_fmt("open %s err"), path);
-		return NULL;
-	}
-
-	log_file *ret = (log_file *)malloc(sizeof(log_file));
-	if (!ret) {
-		err_dbg(0, err_fmt("malloc err"));
-		errno = ENOMEM;
-		close(fd);
-		return NULL;
-	}
-
-	ret->fd = fd;
-	ret->path = path;
-	return ret;
+	return text_file_open(path, O_WRONLY|O_APPEND|O_CREAT, S_IRUSR|S_IWUSR);
 }
 
-int log_write(log_file *file, char *fmt, ...)
+int log_write(log_file *file, log_level level, char *fmt, ...)
 {
 	if (!file) {
 		err_dbg(0, err_fmt("arg check err"));
@@ -37,9 +14,30 @@ int log_write(log_file *file, char *fmt, ...)
 	}
 
 	time_t cur_time = time(NULL);
-	char buf[4096];
-	memset(buf, 0, 4096);
-	sprintf(buf, "%s", asctime(localtime(&cur_time)));
+	char buf[MAXLINE];
+	char *msg = NULL;
+	memset(buf, 0, MAXLINE);
+	switch (level) {
+	case debug:
+		msg = "DEBUG:";
+		break;
+	case info:
+		msg = "INFO: ";
+		break;
+	case warn:
+		msg = "WARN: ";
+		break;
+	case error:
+		msg = "ERROR:";
+		break;
+	case fatal:
+		msg = "FATAL:";
+		break;
+	default:
+		break;
+	}
+	memcpy(buf, msg, strlen(msg));
+	sprintf(buf+strlen(buf), "%s", asctime(localtime(&cur_time)));
 	*(buf+strlen(buf)-1) = '\0';
 	sprintf(buf+strlen(buf), ": ");
 
@@ -48,18 +46,10 @@ int log_write(log_file *file, char *fmt, ...)
 	vsnprintf(buf+strlen(buf), 4096-strlen(buf), fmt, ap);
 	va_end(ap);
 	*(buf+strlen(buf)) = '\n';
-	return write(file->fd, buf, strlen(buf));
+	return write(file->file.fd, buf, strlen(buf));
 }
 
 int log_close(log_file *file)
 {
-	if (!file) {
-		err_dbg(0, err_fmt("arg check err"));
-		errno = EINVAL;
-		return -1;
-	}
-
-	int ret = close(file->fd);
-	free(file);
-	return ret;
+	return text_file_close(file);
 }
